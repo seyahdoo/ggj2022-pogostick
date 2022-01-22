6dd6f9b631aeb83433da4f680fcde4896cad5ed0
Greycher <akgun.talip.ali@gmail.com>
2022-01-22 18:32:19 +0300

    some improvements

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -3,7 +3,8 @@ using UnityEngine.SceneManagement;
 
 public class Finish : MonoBehaviour {
     private void OnTriggerEnter2D(Collider2D other) {
-        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-        SceneManager.LoadScene(currentSceneIndex + 1);
+        var currentIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = (currentIndex + 1) % SceneManager.sceneCountInBuildSettings;
+        SceneManager.LoadScene(nextSceneIndex); 
     }
 }
