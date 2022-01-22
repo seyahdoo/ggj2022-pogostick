83c16773a691e9d8b42e3ea5f60a46bc7953628b
Greycher <akgun.talip.ali@gmail.com>
2022-01-22 12:36:00 +0300

    justify angle implementation

--- Assets/Pogo.cs
+++ Assets/Pogo.cs
@@ -7,11 +7,13 @@ using UnityEngine;
 public class Pogo : MonoBehaviour {
     public Transform rayTransformation;
     public Transform minSpringTransformation;
+    public Transform indicator;
     public float springLength;
     public float bounciness = .8f;
     public float maxPower = 5;
     public float minPower = 1;
     public float jumpWindow = 0.1f;
+    public float maxTorque = 30;
     public LayerMask groundMask;
     
     private RaycastHit2D[] _results = new RaycastHit2D[1];
@@ -33,6 +35,17 @@ public class Pogo : MonoBehaviour {
 
     private void FixedUpdate() {
         BounceDetection();
+        LookAtMouse();
+    }
+
+    private void LookAtMouse() {
+        var plane = new Plane(Vector3.back, Vector3.zero);
+        var ray  = Camera.main.ScreenPointToRay(Input.mousePosition);
+        plane.Raycast(ray, out float distance);
+        var pointedPos = ray.GetPoint(distance);
+        var down = (pointedPos - transform.position).normalized;
+        var targetRot = Quaternion.LookRotation(transform.forward, -down);
+        Rotate2(targetRot.eulerAngles.z);
     }
 
     private void BounceDetection() {
@@ -49,12 +62,14 @@ public class Pogo : MonoBehaviour {
 
             if (distanceToGround <= 0) {
                 Bounce(hit.normal, minPower);
+                indicator.position = minSpringTransformation.position;
             }
             else if(Time.time - _lastJumpInput <= jumpWindow) {
                 var springCenter = springLength / 2;
                 var distanceToCenter = Mathf.Abs(distanceToGround - springCenter);
                 var normDistanceToCenter = distanceToCenter / springCenter;
                 var power = Mathf.Lerp(minPower, maxPower, 1 - normDistanceToCenter);
+                indicator.position = minSpringTransformation.position + minSpringTransformation.forward * distanceToGround;
                 Bounce(hit.normal, power);
             }
         }
@@ -84,5 +99,45 @@ public class Pogo : MonoBehaviour {
         }
         hit = default;
         return false;
+    }
+
+    private void Rotate2(float targetAngle) {
+        var angle = _body.rotation;
+        // Debug.Log($"{nameof(angle)}: {angle}");
+        var deltaAngle = targetAngle - angle;
+        if (deltaAngle > 180f) {
+            deltaAngle -= 360f;
+        }
+        else if (deltaAngle < -180) {
+            deltaAngle += 360f;
+        }
+        var targetAngleVel = deltaAngle / Time.fixedDeltaTime;
+        var deltaAngleVel = targetAngleVel - _body.angularVelocity;
+        var torque = deltaAngleVel * _body.inertia;
+        torque = Mathf.Clamp(torque, -maxTorque, maxTorque);
+        _body.AddTorque(torque, ForceMode2D.Force);
+    }
+    
+    private void Rotate(Quaternion targetRot) {
+        var deltaRot = targetRot * Quaternion.Inverse(transform.rotation);
+        deltaRot.ToAngleAxis(out float angle, out Vector3 axis);
+        
+        if (float.IsInfinity(axis.x)) {
+            axis = Vector3.zero;
+        }
+        
+        if (angle > 180f) {
+            angle -= 360f;
+        }
+        
+        var rad = angle * Mathf.Deg2Rad; 
+        var targetAngularVel = axis * rad / Time.deltaTime; 
+        var deltaAngularVel = targetAngularVel - new Vector3(0, 0, _body.angularVelocity);
+        var torque = Vector3.Scale(deltaAngularVel, new Vector3(0, 0, _body.inertia));
+        _body.AddTorque(torque.z, ForceMode2D.Impulse);
+    }
+
+    private float JustifyAngle(float angle) {
+        return (angle % 360 + 360) % 360;
     }
 }
