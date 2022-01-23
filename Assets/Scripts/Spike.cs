261c58f949415cae63b976833aeebacb98b942aa
ytkimirti <yusuftaha9@gmail.com>
2022-01-23 15:32:51 +0300

    pogo fix

--- Assets/Scripts/Spike.cs
+++ Assets/Scripts/Spike.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class Spike : MonoBehaviour {
     [SerializeField] private float sceneReloadDelay;
     private void OnTriggerEnter2D(Collider2D collider) {
+        if (collider.gameObject.tag == "Derby")
+            return;
         collider.gameObject.GetComponentInParent<PhysicsExploder>().Explode();
         // collider.attachedRigidbody.gameObject.SetActive(false);
         StartCoroutine(SceneLoader.ReloadSceneWithDelay(sceneReloadDelay));
